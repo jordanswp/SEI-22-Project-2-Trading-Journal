6d9f727e4cd4db6c10aa9e1e672c9b54438cd30b
jordanswp <sohweepin@gmail.com>
2020-06-24 02:59:37 +0800

    edited login page

--- views/login.jsx
+++ views/login.jsx
@@ -12,9 +12,10 @@ class Login extends React.Component {
       <Head />
         <body style={{backgroundImage:`url('/assets/bg.jpg')`, backgroundRepeat:'no-repeat', backgroundSize:'cover' }}>
         <Nav />
-            <div className="container" style={{background: 'white' }}>
-                <div class="row">
-                    <div class="col-sm-4 col-md-offset-3" style={{backgroundColor:'white', boxShadow: '0 0 50px grey', position:'absolute', top:'200px'}}>
+
+            <div className="container" style={{background: 'transparent' }}>
+
+                    <div class="col-sm-4 col-md-offset-4" style={{backgroundColor:'white', boxShadow: '0 0 50px grey', marginTop: '150px'}}>
                         <h1 style={{textAlign:'center'}}>Login</h1>
                         <div>
                             <form action="/login" method="POST">
@@ -34,14 +35,16 @@ class Login extends React.Component {
                                 <p>
                                     <input className="btn btn-info btn-block" type="submit"/>
                                 </p>
+
                             </form>
                                 <p>
                                     Don't have an account? <a href="/register">Sign Up Here</a>
                                 </p>
                         </div>
                     </div>
-                  </div>
+
               </div>
+
           <Footer2 />
         </body>
       </html>
